8ca5070e32b99fe1bb259dc34cc5ac650f0c513c
nergal-perm <nergal@psu.ru>
2015-07-06 12:46:42 +0500

    Added ability to download Flibusta books
    Requires Tor and Privoxy to work

---
+++ UnitTests/ProxifiedConnection.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: terekhov-ev
+ * Date: 06.07.2015
+ * Time: 8:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Net;
+
+namespace book2read.UnitTests {
+	/// <summary>
+	/// Description of ProxifiedConnection.
+	/// </summary>
+	public class ProxifiedConnection {
+		WebClient client;
+		WebProxy wp;
+		
+		public ProxifiedConnection() {
+			client = new WebClient();
+			wp = new WebProxy("127.0.0.1", 8118);
+			client.Proxy = wp;
+		}
+		
+		public bool DownloadFile(string fileUrl) {
+			string fileName = string.Empty;
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(fileUrl);
+			request.Proxy = wp;
+			WebResponse response = request.GetResponse();
+			string contentDisposition = request.Address.AbsoluteUri;
+			if (!string.IsNullOrEmpty(contentDisposition)) {
+				string lookFor = "/";
+				int index = contentDisposition.LastIndexOf(lookFor, StringComparison.CurrentCultureIgnoreCase);
+				if (index > 0)
+					fileName = contentDisposition.Substring(index+1);
+			}
+			if (fileName.Length > 0) {
+				client.DownloadFile(fileUrl, @"D:\Temp\" + fileName);
+			} else {
+				client.DownloadFile(fileUrl, @"D:\Temp\flibusta.fb2");
+			}
+			
+			return true;
+		}
+	}
+}
---
+++ UnitTests/ProxifiedConnectionTest.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: terekhov-ev
+ * Date: 06.07.2015
+ * Time: 8:56
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace book2read.UnitTests
+{
+	[TestFixture]
+	public class ProxifiedConnectionTest
+	{
+		[Test]
+		public void TestMethod()
+		{
+			var target = new ProxifiedConnection();
+			Assert.IsTrue(target.DownloadFile(@"http://flibustahezeous3.onion/b/165506/fb2"));
+		}
+	}
+}
